f711c66f11d3a14d86d07a4da34200ba77f4d3dc
leticia-chijo <leticia.chijo@gmail.com>
2023-03-07 15:06:35 -0300

    Correção nos data tests dos ícones

--- src/components/StatusIcon.js
+++ src/components/StatusIcon.js
@@ -6,10 +6,10 @@ import wrongIcon from "../assets/icone_erro.png"
 export default function StatusIcon({ status, showQuestion }) {
     return (
         <>
-            {status === "correct" && <img src={correctIcon} alt="Ícone de certo" data-test="play-btn"/>}
+            {status === "correct" && <img src={correctIcon} alt="Ícone de certo" data-test="zap-btn"/>}
             {status === "almost" && <img src={almostIcon} alt="Ícone de quase" data-test="partial-icon"/>}
             {status === "wrong" && <img src={wrongIcon} alt="Ícone de errado" data-test="no-icon"/>}
-            {status === "not answered" && <img onClick={showQuestion} src={play} alt="Ícone de play" data-test="zap-icon"/>}
+            {status === "not answered" && <img onClick={showQuestion} src={play} alt="Ícone de play" data-test="play-icon"/>}
         </>
     )
 }
